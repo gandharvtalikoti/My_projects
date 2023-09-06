ec3d3e1e94da46407aa65f161f15eef859950894
Gandharv Talikoti <gandharvtalikoti19@gmail>
2023-09-06 19:12:01 +0530

    basic idealogy

--- Robo go to home/RobotMazePrpblem.py
+++ Robo go to home/RobotMazePrpblem.py
@@ -1,3 +1,20 @@
 # Backtracking
 
-maze = [[".",".",".","."],[]]
+maze = [[".", ".", ".", "."],
+        [".", "X", "X", "X"],
+        [".", ".", ".", "X"],
+        ["X", "X", ".", "."]]
+
+
+def print_maze(maze):
+    return
+
+
+def solve_maze(maze):
+    return
+
+
+"""
+> solve_maze(maze)
+['d', 'd', 'r', 'r','d','r']
+"""
